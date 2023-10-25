651008580aabc237c37140965ac94c3acf797647
Guilherme Da Silva Vieira <guilhermedas.vieira@gmail.com>
2023-10-25 12:19:52 -0300

    add: spotify adblock

--- home_manager/files.nix
+++ home_manager/files.nix
@@ -58,5 +58,19 @@
         
         exec awesome
       '';
-     };
+
+    "${config.home.homeDirectory}/.local/share/applications/spotify-adblock.desktop".text = '' 
+        [Desktop Entry]
+        Type=Application
+        Name=Spotify (adblock)
+        GenericName=Music Player
+        Icon=spotify-client
+        TryExec=spotify
+        Exec=env LD_PRELOAD=/usr/local/lib/spotify-adblock.so spotify %U
+        Terminal=false
+        MimeType=x-scheme-handler/spotify;
+        Categories=Audio;Music;Player;AudioVideo;
+        StartupWMClass=spotify      
+      '';
+   };
 }
