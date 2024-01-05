169fd3d3d7486a2c109fd779d8485866f4992470
Guilherme Da Silva Vieira <guilhermedas.vieira@gmail.com>
2024-01-05 19:28:51 -0300

    better battery life

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -357,6 +357,8 @@ in  {
     keyMap = "br-abnt2";
   };
 
+  powerManagement.cpuFreqGovernor = "powersave";
+
   # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
   system.stateVersion = "23.11";
 }
