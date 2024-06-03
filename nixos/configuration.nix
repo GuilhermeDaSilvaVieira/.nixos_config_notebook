5629740f9aeb51dbd24f89f6eecc38c19981cff9
Guilherme Da Silva Vieira <guilhermedas.vieira@gmail.com>
2024-06-03 17:25:40 -0300

    update

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -364,5 +364,5 @@ in  {
   # powerManagement.cpuFreqGovernor = "powersave";
 
   # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
-  system.stateVersion = "23.11";
+  system.stateVersion = "24.11";
 }
