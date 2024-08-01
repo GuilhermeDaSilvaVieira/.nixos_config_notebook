3726accf42867c8d4328f2555d391ae26da53f8b
Guilherme Da Silva Vieira <guilhermedas.vieira@gmail.com>
2024-08-01 16:07:41 -0300

    bye bye opengl, welcome graphics

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -288,10 +288,8 @@ in  {
     enable = true;
   };
 
-  hardware.opengl = {
+  hardware.graphics = {
     enable = true;
-    driSupport = true;
-    driSupport32Bit = true;
     extraPackages = with pkgs; [
       intel-media-driver # LIBVA_DRIVER_NAME=iHD
       vaapiIntel         # LIBVA_DRIVER_NAME=i965 (older but works better for Firefox/Chromium)
