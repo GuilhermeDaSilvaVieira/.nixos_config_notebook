49b0c69b8950ac4cb48051cea587fa6afc69a6ca
Guilherme Da Silva Vieira <guilhermedas.vieira@gmail.com>
2024-10-15 19:16:30 -0300

    update

--- flake.nix
+++ flake.nix
@@ -19,7 +19,7 @@
     # Shameless plug: looking for a way to nixify your themes and make
     # everything match nicely? Try nix-colors!
     # nix-colors.url = "github:misterio77/nix-colors";
-    zen-browser.url = "github:MarceColl/zen-browser-flake";
+    zen-browser.url = "github:ch4og/zen-browser-flake";
   };
 
   outputs =
